8c59bb18c1f2a202ff94bb4d3f322129d4566ad1
rsamdaan <rsamdaan@student.wethinkcode.co.za>
2020-11-10 15:18:40 +0200

    added and improved functions.

--- calendar_test.py
+++ calendar_test.py
@@ -1,3 +1,10 @@
+'''To create vitual environment and install necessary packages:
+        python3 -m venv vcalendar
+        source /goinfre/rsamdaan/practice_team_08/vcalendar/bin/activate
+        pip install google-api-python-client
+        pip install --upgrade oauth2client
+'''
+
 from __future__ import print_function
 from apiclient.discovery import build
 from httplib2 import Http
@@ -18,6 +25,7 @@ if not creds or creds.invalid:
     flow = client.flow_from_clientsecrets("client_secret.json", SCOPES)
     creds = tools.run_flow(flow, store, flags) \
         if flags else tools.run(flow, store)
+
 CAL = build("calendar", "v3", http=creds.authorize(Http()))
 
 GMT_OFF = "+02:00"
@@ -29,36 +37,62 @@ EVENT = {
 }
 
 
-'''adds event'''
-# e = CAL.events().insert(calendarId="primary",sendNotifications=True, body=EVENT).execute()
-# print('''*** %r event added:
-#     Start: %s
-#     End:   %s''' % (e["summary"].encode("utf-8"),
-#         e["start"]["dateTime"], e["end"]["dateTime"]))
+'''Adds event
+time needs to be string of format 2020-11-11T09:00:00+02:00'''
+def add_event(summary, start_time, end_time, email):
+    EVENTS = {
+    "summary": summary,
+    "start": {"dateTime": start_time},
+    "end":   {"dateTime": end_time},
+    "attendees": [email],
+    }
+    e = CAL.events().insert(calendarId="primary",sendNotifications=True, body=EVENTS).execute()
+
+    print('''*** %r event added:
+        Start: %s
+        End:   %s''' % (e["summary"].encode("utf-8"),
+            e["start"]["dateTime"], e["end"]["dateTime"]))
 
 
-'''displays next 10 events'''
-# now = datetime.datetime.utcnow().isoformat() + 'Z'
-# events_result = CAL.events().list(calendarId='primary', timeMin=now,
-#                                         maxResults=10, singleEvents=True,
-#                                         orderBy='startTime').execute()
-# events = events_result.get('items', [])
+'''Displays next 7 days'''
+def display_event():
+    now = datetime.datetime.utcnow().isoformat() + 'Z'
+    elapsed = datetime.timedelta(days=7)
+    then = (datetime.datetime.utcnow() + elapsed).isoformat() + 'Z'
 
-# for event in events:
-#         start = event['start'].get('dateTime', event['start'].get('date'))
-#         print(start, event['summary'])
+    events_result = CAL.events().list(calendarId='primary', timeMax=then, timeMin=now,
+                                            singleEvents=True,
+                                            orderBy='startTime').execute()
+    events = events_result.get('items', [])
+
+    for event in events:
+            start = event['start'].get('dateTime', event['start'].get('date'))
+            print(start, event['summary'])
+
+    # TODO add code to store data
 
 
-'''deletes event by ID // more options, troubleshoot info'''
-# CAL.events().delete(calendarId='primary', eventId='am94eme402tr17aokvba3nnlo4').execute()
+'''
+Deletes event by ID
+Will need to do more research on deleting specific events
+'''
+def cancel_event(eventID):
+    CAL.events().delete(calendarId='primary', eventId=eventID).execute()
 
 
 '''shows event name and id'''
-page_token = None
-while True:
-  events = CAL.events().list(calendarId='primary', pageToken=page_token).execute()
-  for event in events['items']:
-    print(event["summary"], event["id"])
-  page_token = events.get('nextPageToken')
-  if not page_token:
-    break
+def show_id():
+    page_token = None
+    now = datetime.datetime.utcnow().isoformat() + 'Z'
+
+    while True:
+        events = CAL.events().list(calendarId='primary',timeMin=now, pageToken=page_token).execute()
+        for event in events['items']:
+            print(event["summary"], event["id"])
+        page_token = events.get('nextPageToken')
+        if not page_token:
+            break
+
+
+# add_event("These things", "2020-11-11T09:00:00+02:00", "2020-11-11T11:00:00+02:00", "roderick@mail")
+display_event()
