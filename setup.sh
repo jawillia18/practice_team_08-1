2c174621178ca8194f7dd81df413da4a0b1f8249
rsamdaan <rsamdaan@student.wethinkcode.co.za>
2020-11-30 09:58:27 +0200

    Setup updated

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-
+# setopt extendedglob
 # create a new home directory
-mkdir ~/code_clinics_team8
+# mkdir ~/code_clinics_team8
 
 # copy code_clinics directory to the new home directory
-cp -r code_clinics8 ~/code_clinics_team8
+# cp -r !(.git) ~/code_clinics_team8
 
 # install neccesary packages and modules
 python3 -m pip install pip
@@ -16,9 +16,9 @@ python3 -m pip install google_auth_oauthlib
 python3 -m pip install bcrypt
 
 # these will configure all the aliases
-echo "alias cs8='cd ~/code_clinics_team8/code_clinics8/ && python3 ~/code_clinics_team8/code_clinics8/main.py'" >> ~/.zshrc # this cmd will add the alias to the zsh terminal
+echo "alias cs8='python3 main_for_commandline.py'" >> ~/.zshrc # this cmd will add the alias to the zsh terminal
 
-echo "alias cs8='cd ~/code_clinics_team8/code_clinics8/ && python3 ~/code_clinics_team8/code_clinics8/main.py'" >> ~/.bashrc # this cmd will add the alias to the bash terminal
+echo "alias cs8='python3 main_for_commandline.py'" >> ~/.bashrc # this cmd will add the alias to the bash terminal
 
 # all parkages have been installed
 # execute the programme
